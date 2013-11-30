defc923557c52c13408b611fb85fb2a04649d7ce
Blair Williams <blair.l.williams@intel.com>
2013-11-29 17:06:35 -0800

    Testing changes

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -2,6 +2,7 @@ Spree::Admin::OrdersController.class_eval do
   def label
     begin
       label = Spree::ShippingLabel.new params[:shipment_id]
+      RAILS.logger.info params
     rescue Exception => e
       flash[:error] = e.message
       redirect_to :back
